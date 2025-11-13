7793875b23f7a4d3df1d6040b5b3c6b666ad046e
mahajansteel <mahajansteeldinanagar@gmail.com>
2025-11-13 21:06:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("show");
+}
+function openWebsite() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/ceiling-fan-box.html"); 
+    }
+function openWebsite2() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/ceiling-fan-clamp.html"); 
+    }
+function openWebsite3() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/ceiling-fan-downrod.html"); 
+    }
+function openWebsite4() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/mcb-box.html"); 
+    }
+function openWebsite5() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/electrical-box.html"); 
+    }
+function openWebsite6() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/conduit-pipe.html"); 
+    }
+function openWebsite7() {
+      window.open("https://www.indiamart.com/mahajansteelenterprises/ms-conduit-pipes.html"); 
+    }
+  
+
+
+
+
